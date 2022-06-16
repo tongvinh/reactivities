f2f0279419e4b7952350a44c0f68ff4420f092d9
Vinh Tong <tonggiavinh2010@gmail.com>
2022-06-16 15:53:43 +0700

    End of Section $

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Extensions
+{
+  public static class ApplicationServiceExtensions
+  {
+    public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+    {
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+      });
+
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+        });
+      });
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+      return services;
+    }
+    public static async Task<WebApplicationBuilder> AddMirageAndSeedData(this WebApplicationBuilder builder, WebApplication app)
+    {
+      using var scope = app.Services.CreateScope();
+      var services = scope.ServiceProvider;
+      try
+      {
+        var context = services.GetRequiredService<DataContext>();
+        await context.Database.MigrateAsync();
+        await Seed.SeedData(context);
+      }
+      catch (Exception ex)
+      {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred during migration");
+      }
+      return builder;
+    }
+  }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,7 @@
 using System;
+using API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -19,23 +19,14 @@ var env = builder.Environment;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt =>
-{
-  opt.AddPolicy("CorsPolicy", policy =>
-  {
-    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-  });
-});
+//Extend to Extension MyCustom
+builder.Services.AddApplicationService(config);
 
 var app = builder.Build();
 
 //Start custom auto update database 
-using var scope = app.Services.CreateScope();
+/* using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 try
 {
@@ -47,8 +38,8 @@ catch (Exception ex)
 {
   var logger = services.GetRequiredService<ILogger<Program>>();
   logger.LogError(ex, "An error occurred during migration");
-}
-
+} */
+await builder.AddMirageAndSeedData(app);
 //End custom auto update database 
 
 // Configure the HTTP request pipeline.
