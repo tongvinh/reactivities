e5f0063237ce3827d4ab6bb6b9f17237430ca85e
Vinh Tong <tonggiavinh2010@gmail.com>
2022-06-16 13:08:21 +0700

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -24,6 +24,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
   opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt =>
+{
+  opt.AddPolicy("CorsPolicy", policy =>
+  {
+    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+  });
+});
+
 var app = builder.Build();
 
 //Start custom auto update database 
@@ -53,6 +61,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
