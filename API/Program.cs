e3404da39da2f80bc9a8dd7e8d0cfbd861ff2e85
Vinh Tong <tonggiavinh2010@gmail.com>
2022-07-07 16:50:54 +0700

    fix bug

--- API/Program.cs
+++ API/Program.cs
@@ -64,7 +64,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
   .BlockAllMixedContent()
   .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdn.jsdelivr.net", "data:", "sha256-yR2gSI6BIICdRRE2IbNP1SJXeA5NYPbaM32i/Y8eS9o="))
   .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", " https://cdn.jsdelivr.net", "data:"))
